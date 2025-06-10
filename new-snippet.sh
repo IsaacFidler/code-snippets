ba6dd8515a0e3b9bc94df4e442bf0b67f87b2de0
IsaacFidler <isaac.fidler@bx-earth.com>
2025-06-10 12:06:20 +0100

    Remove inline comment from new-snippet.sh

--- new-snippet.sh
+++ new-snippet.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-# Resolve the directory where this script resides
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 read -rp "Category: " CATEGORY
